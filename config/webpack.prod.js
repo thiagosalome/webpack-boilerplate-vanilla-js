8a21cdaf47240fb2980e6d6b06361d324251ac46
Thiago Salome <thiagosalome@gmail.com>
2021-12-31 10:30:02 -0300

    more cofigurations

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,36 +2,23 @@ const path = require("path");
 const merge = require("webpack-merge");
 const common = require("./webpack.common");
 const cleanWebpackPlugin = require("clean-webpack-plugin");
-const uglifyjsWebpackPlugin = require("uglifyjs-webpack-plugin");
 const copyWebpackPlugin = require("copy-webpack-plugin");
-
-const tagBuildCss = require("./package.json")['tag-build-css'];
-const tagBuildJS = require("./package.json")['tag-build-sprite'];
 
 module.exports = merge(common, {
   mode: 'production',
-  optimization: {
-    minimizer: [
-      new uglifyjsWebpackPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true
-      }),
-    ]
-  },
   plugins: [
-    new cleanWebpackPlugin(["dist"]),
+    new cleanWebpackPlugin(["build"]),
     new copyWebpackPlugin([
       {
         from: path.resolve(__dirname, "src/images/"),
-        to: path.resolve(__dirname, 'dist/images'),
+        to: path.resolve(__dirname, 'build/images'),
         ignore: ['svgs/*'],
         cache: true
       }
     ]),
   ],
   output: {
-    filename : `src/scripts/[name].${tagBuildJS}.min.js`,
-    path: path.resolve(__dirname, 'dist')
+    filename : `src/scripts/app.min.js`,
+    path: path.resolve(__dirname, 'build')
   }
 });
